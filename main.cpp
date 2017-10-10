62e285c72c5b694c8ce9eca26de13d091be8dbf1
Malnassafi <malnassafi@mail.csuchico.edu>
2017-10-09 23:56:01 -0700

    18

--- main.cpp
+++ main.cpp
@@ -8,7 +8,14 @@ int main()
     vector <vector <int> > multiTable; 
     
     multiTable.resize(13); 
-
+    for(int r1=0; r1 < multiTable.size(); r1++)
+        multiTable[r1].resize(13);
+        cout<<"X";
+    for(int fr=1; fr<12; fr++)
+    {       
+        cout<<" "<<fr;
+    }
+    cout<<endl;
     for(int r=1; r < multiTable.size(); r++)
     {  
         multiTable[r].resize(13);
